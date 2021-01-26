a807d6108f1d5005bbb8da84b6c921ce7928f599
RithvikC <rithvikc@ad.unc.edu>
2021-01-26 16:29:23 -0500

    stage 2

--- lessons/diagram_practice.py
+++ lessons/diagram_practice.py
@@ -4,6 +4,6 @@ name: str = input("First Name? ")
 adjective: str = input("Adjective? ")
 body_part: str = input("Favorite body part? ")
 
-name = "Cap'n" + adjective + " " + body_part + " " +name
+name = "Cap'n" + " " + adjective + " " + body_part + " " +name
 
 print("Your pirate name is " + name)
