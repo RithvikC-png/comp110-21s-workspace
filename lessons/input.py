13443450750e04bd62b6f40337c837994135da7f
RithvikC <rithvikc@ad.unc.edu>
2021-01-26 16:11:42 -0500

    stage 2

--- lessons/input.py
+++ lessons/input.py
@@ -1,4 +1,4 @@
 """A program to demonstrate user input and variables."""
 
-print("Wow, " + input("Who are you?")+", you rock!")
-print(input(Who are you? ") + "have a great day!")
+print("Wow, " + input("Who are you? ") + ", you rock!")
+print(input("Who are you? ") + " have a great day!")
