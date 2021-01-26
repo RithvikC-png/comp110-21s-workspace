b62aea25e833d397369f3c369e8b43d890395c21
RithvikC <rithvikc@ad.unc.edu>
2021-01-26 16:54:23 -0500

    stage 4

--- lessons/diagram_submit.py
+++ lessons/diagram_submit.py
@@ -5,3 +5,6 @@ year: int = int(input("What year is it? "))
 age_in_2041: int = 2041 - year + age
 print("In 2041, you will be" + str(age_in_2041)
 
+age = age + 1
+year = year + 1
+print("In " + str(year) + ", you'll be " + str(age))
