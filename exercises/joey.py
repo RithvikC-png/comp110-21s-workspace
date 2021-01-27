1fa277bcbdc63fa9b2f607de11deb901d32eab9a
RithvikC <rithvikc@ad.unc.edu>
2021-01-27 14:18:36 -0500

    4

--- exercises/joey.py
+++ exercises/joey.py
@@ -1,7 +1,7 @@
 """Should Joey go to class"""
 
 import random
-answer: print(random.randint(0,1))
+print(random.randint(0,1))
 
-if(answer = 0) print("No")
-if(answer = 1) print("Yes")
+0 = print("No")
+1 = print("Yes")
