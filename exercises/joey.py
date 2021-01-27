70752f0bc981a0580b2e26d4e147cd4ab32ba9f1
RithvikC <rithvikc@ad.unc.edu>
2021-01-27 15:30:42 -0500

    15

--- exercises/joey.py
+++ exercises/joey.py
@@ -8,11 +8,11 @@ Q3: str = (input("Is you laptop too far away? "))
 
 score = int(0)
 
-if input(Q1) = str(yes)
+if input(Q1) = 1
     score = score + 1
-if input(Q2) = str(yes)
+if input(Q2) = 1
     score = score + 1
-if input(Q3) = str(yes)
+if input(Q3) = 1
     score = score + 1
 
 if score >= 2
