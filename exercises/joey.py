3690df6a60415b23c5b5637abcf2963bf4477cf1
RithvikC <rithvikc@ad.unc.edu>
2021-01-27 18:33:39 -0500

    19

--- exercises/joey.py
+++ exercises/joey.py
@@ -1,12 +1,12 @@
 """Should you go to class"""
 
+score: int = int(0)
+
 name: str = input("What is your name? ")
 
 Q1 = input("Are you tired right now? ")
 Q2 = input("Do you hate class? ")
 Q3 = input("Is you laptop too far away? ")
-
-score = int(0)
 
 if Q1 == "yes"
     score = score + 1
