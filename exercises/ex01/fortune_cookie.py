2170ccec2bf8ff0a465f4c828a687617ce92ac4f
RithvikC <rithvikc@ad.unc.edu>
2021-02-05 00:54:01 -0500

    16

--- exercises/ex01/fortune_cookie.py
+++ exercises/ex01/fortune_cookie.py
@@ -17,13 +17,6 @@ print("Your fortune cookie says...")
 
 a = random.randint(1,4)
 
-if a < 2
-    print("The early bird gets the worm, but the second mouse gets the cheese")
-    elif a < 3
-        print("A faithful friend is a strong defense")
-        elif a < 4
-            print("A fresh start will put you on your way")
-            else
-                print("A friend asks only for your time not your money")
+print(a)
 
 print("Now, go spread positive vibes!")
