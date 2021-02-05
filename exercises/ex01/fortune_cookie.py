db2369a6ad6d42bfe2e99639592c855a5d6fa28e
RithvikC <rithvikc@ad.unc.edu>
2021-02-05 00:28:02 -0500

    4

--- exercises/ex01/fortune_cookie.py
+++ exercises/ex01/fortune_cookie.py
@@ -15,4 +15,14 @@ from random import randint
 # Begin your solution here...
 print("Your fortune cookie says...")
 
+a: from random import randint(1,4)
+if a < 2
+    print("The early bird gets the worm, but the second mouse gets the cheese")
+elif a < 3
+    print("A faithful friend is a strong defense")
+elif a < 4
+    print("A fresh start will put you on your way")
+else
+    print("A friend asks only for your time not your money")
+
 print("Now, go spread positive vibes!")
