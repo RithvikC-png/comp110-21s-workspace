5dbecd7b05cc4f34bc4b0db2f9b10fc342042681
RithvikC <rithvikc@ad.unc.edu>
2021-02-05 01:13:37 -0500

    pid

--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -1,6 +1,6 @@
 """A vaccination calculator."""
 
-__author__ = "YOUR PID HERE"
+__author__ = "730398645"
 
 # The datetime data type is imported from the datetime library.
 # A datetime object models a specific date and time.
@@ -18,3 +18,4 @@ from datetime import timedelta
 
 
 # Begin your solution here...
+
