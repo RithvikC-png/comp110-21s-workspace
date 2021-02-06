68bd6bc168362e1cb9d4ecb24487277397a9222a
RithvikC <rithvikc@ad.unc.edu>
2021-02-06 17:23:57 -0500

    20

--- exercises/joey.py
+++ exercises/joey.py
@@ -1,21 +1,16 @@
 """Should you go to class"""
 
-score: int = int(0)
-
 name: str = input("What is your name? ")
 
 Q1 = input("Are you tired right now? ")
 Q2 = input("Do you hate this class? ")
 Q3 = input("Is you laptop too far away? ")
 
-if input(Q1) == "yes" or "Yes":
-    score = score + int(1)
-if input(Q2) == "yes" or "Yes":
-    score = score + int(1)
-if input(Q3) == "yes" or "Yes":
-    score = score + int(1)
-
-if score >= 2:
-    print("You should not go to class " + name)
-if score < 2:
-    print("You should go to class " + name)
+if input(Q1) == "no" or "No":
+    print("Go to class " + name)
+elif input(Q2) == "yes" or "Yes":
+    print("Go to class " + name)
+elif input(Q3) == "yes" or "Yes":
+    print("Go to class " + name)
+else:
+    print("You should no go to class " + name)
