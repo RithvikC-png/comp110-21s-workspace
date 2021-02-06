693d8cb4d8042fef85e5fe72cc0efadda336c705
RithvikC <rithvikc@ad.unc.edu>
2021-02-06 16:54:40 -0500

    13

--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -23,6 +23,8 @@ from datetime import datetime, timedelta
 
 today: datetime = datetime.today()
 
-print(today.strftime("%B %d, %Y"))
-
 one_day: timedelta = timedelta(1)
+
+tomorrow: datetime = today + one_day
+
+print(tomorrow.strftime("%B %d, %Y"))
