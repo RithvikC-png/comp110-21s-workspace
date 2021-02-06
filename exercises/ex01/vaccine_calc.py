786d176a1c9243e69e79c8088579ccb269e0afe6
RithvikC <rithvikc@ad.unc.edu>
2021-02-06 16:59:21 -0500

    16

--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -32,4 +32,4 @@ today: datetime = datetime.today()
 plus_days: timedelta = timedelta(days_to_target)
 target: datetime = today + plus_days
 
-print(target.strftime("%B %d, %Y"))
+print("We will reach" + " target_per in" + " days_to_target which falls on" + target.strftime("%B %d, %Y"))
