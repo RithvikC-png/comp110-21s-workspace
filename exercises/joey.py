76a31233b09a345bfd71b313e4ee83acc8ed4c42
RithvikC <rithvikc@ad.unc.edu>
2021-02-07 00:11:20 -0500

    24

--- exercises/joey.py
+++ exercises/joey.py
@@ -6,11 +6,11 @@ Q1 = input("Are you tired right now? ")
 Q2 = input("Do you hate this class? ")
 Q3 = input("Is you laptop too far away? ")
 
-if input(Q1) = "no" or "No":
-    print("Go to class " + name)
-elif input(Q2) = "no" or "No":
-    print("Go to class " + name)
-elif input(Q3) = "no" or "No":
-    print("Go to class " + name)
+if input(Q1) == "no" or "No":
+    print("Go to class " + name) break
+elif input(Q2) == "no" or "No":
+    print("Go to class " + name) break
+elif input(Q3) == "no" or "No":
+    print("Go to class " + name) break
 else:
     print("You should not go to class " + name)
