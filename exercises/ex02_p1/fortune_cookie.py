d6d345f396e46ca2592c14852df338bcb5cded19
RithvikC <rithvikc@ad.unc.edu>
2021-02-12 00:23:56 -0500

    1

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -2,7 +2,7 @@
 
 from random import randint
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730398645"
 
 
 def main() -> None:
@@ -11,11 +11,30 @@ def main() -> None:
     # TODO 2: Print the result of calling your fortune_cookie function.
     print("Now, go spread positive vibes!")
 
+def fortune_cookie() -> str:
+    a = randint(1, 4)
+
+    if a < 2:
+        return("The early bird gets the worm, but the second mouse gets the cheese")
+    elif a < 3:
+        return("A faithful friend is a strong defense")
+    elif a < 4:
+        return("A fresh start will put you on your way")
+    else:
+        return("A friend asks only for your time not your money")
+
 
 # TODO 1: Define your fortune_cookie function here.
-
+print(fortune_cookie())
 
 # Python Idiom for "starting" the program when run as a module.
 # The special dunder variable __name__ will be "__main__" when run as module. 
+
+
+
 if __name__ == "__main__":
     main()
+
+
+
+
