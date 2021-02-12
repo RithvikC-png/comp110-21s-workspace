bc4729e272fdb7068b4240f1a4cb5118687c35d1
RithvikC <rithvikc@ad.unc.edu>
2021-02-12 00:27:39 -0500

    1

--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -7,10 +7,21 @@ def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
     # TODO 2: Print the response of calling the tar_heels function here.
-
+print(tar_heels(unc))
 
 # TODO 1: Define the tar_heels function, and its logic, here.
+def tar_heels(unc: int) -> str:
 
+    a = int(input("Enter a number "))
+
+    if (a % 2) == 0 and (a % 7) == 0:
+        print("TAR HEELS")
+    elif (a % 2) == 0:
+        print("TAR")
+    elif (a % 7) == 0:
+        print("HEELS")
+    else:
+        print("CAROLINA")
 
 if __name__ == "__main__":
     main()
