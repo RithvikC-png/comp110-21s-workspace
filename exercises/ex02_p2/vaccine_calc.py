922f48684aafbed92a826507c801818f36cc71d6
RithvikC <rithvikc@ad.unc.edu>
2021-02-17 23:12:10 -0500

    1

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -11,16 +11,42 @@ def main() -> None:
     doses: int = int(input("Doses administered: "))
     doses_per_day: int = int(input("Doses per day: "))
     target: int = int(input("Target percent vaccinated: "))
+
+    today: datetime = datetime.today()
+
     # TODO 2: Call days_to_target and store result in a variable.
+    return(days_to_target)
+    days_to = days_to_target
     # TODO 4: Call future_date and store result in a variable.
+    return(future_date)
     # TODO 5: Print the expected output using the variables above.
 
+    plus_days: timedelta = timedelta(days_to)    
+
+    x = "We will reach " + str(target)
+    y = "% vaccination in " + str(days_to_target)
+    z = " days, which falls on " + str(future_date.strftime("%B %d, %Y"))
+
+    print(x + y + z)
 
 # TODO 1: Define days_to_target function
+def days_to_target(days: int) -> int:
+    """The number of days to the target vaccination"""
+    days = days
 
+    int(round(((target / int(100) * population * int(2)) - doses) / doses_per_day))
 
 # TODO 3: Define future_date function
+def future_date(date: int) -> int:
+    """The date of target vaccination"""
+    date = date
 
+    datetime = today + plus_days
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
