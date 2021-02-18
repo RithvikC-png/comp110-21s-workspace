b40912a23cea1807f3a6ab2f79830a3397640254
RithvikC <rithvikc@ad.unc.edu>
2021-02-18 01:49:14 -0500

    5

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -7,39 +7,40 @@ __author__ = "730398645"
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
+    
     population: int = int(input("Population: "))
     doses: int = int(input("Doses administered: "))
     doses_per_day: int = int(input("Doses per day: "))
     target: int = int(input("Target percent vaccinated: "))
-
-    today: datetime = datetime.today()
+    
 
     # TODO 2: Call days_to_target and store result in a variable.
-    return(days_to_target)
-    days_to = days_to_target
+    a = days_to_target
+
     # TODO 4: Call future_date and store result in a variable.
+    c = future_date
     return(future_date)
+
     # TODO 5: Print the expected output using the variables above.
-
-    plus_days: timedelta = timedelta(days_to)    
-
-    x = "We will reach " + str(target)
-    y = "% vaccination in " + str(days_to_target)
-    z = " days, which falls on " + str(future_date.strftime("%B %d, %Y"))
-
-    print(x + y + z)
+    print(days_to_target)
 
 # TODO 1: Define days_to_target function
-def days_to_target() -> None:
+def days_to_target(population: int, doses: int, doses_per_day: int, target: int) -> int:
     """The number of days to the target vaccination"""
 
-    days_to_target = int(round(((target / int(100) * population * int(2)) - doses) / doses_per_day))
+    dtg: int(round(((target / int(100) * population * int(2)) - doses) / doses_per_day))
+
+    today: datetime = datetime.today()
+    b: timedelta = timedelta(dtg)
+    g: datetime = today + b
+    
+    x = "We will reach " + str(target)
+    y = "% vaccination in " + str(a)
+    z = " days, which falls on " + str(g.strftime("%B %d, %Y"))
 
 # TODO 3: Define future_date function
-def future_date() -> None:
+def future_date(datetime: int) -> str:
     """The date of target vaccination"""
-
-    datetime = today + plus_days
 
 if __name__ == "__main__":
     main()
