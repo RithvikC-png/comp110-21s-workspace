e3d412cbd4414abb9d21c7c98b2853973bafb3f6
RithvikC <rithvikc@ad.unc.edu>
2021-02-18 22:59:42 -0500

    todo 2 test

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -14,11 +14,14 @@ def main() -> None:
 
 
     # TODO 2: Call days_to_target and store result in a variable.
+    a = days_to_target(population, doses, doses_per_day, target)
+    print(a)
     # TODO 4: Call future_date and store result in a variable.
     # TODO 5: Print the expected output using the variables above.
 
 # TODO 1: Define days_to_target function
-
+def days_to_target(population: int, doses: int, doses_per_day: int, target: int) -> int:
+    days = int(round((target / int(100) * population * int(2)) - doses) / doses_per_day)
 # TODO 3: Define future_date function
 
 if __name__ == "__main__":
