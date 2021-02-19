e9a8261f9acf4d011760877a6528cea81112423d
RithvikC <rithvikc@ad.unc.edu>
2021-02-18 23:20:50 -0500

    complete p2

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -4,14 +4,12 @@ from datetime import datetime, timedelta
 
 __author__ = "730398645"
 
-
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     population: int = int(input("Population: "))
     doses: int = int(input("Doses administered: "))
     doses_per_day: int = int(input("Doses per day: "))
     target: int = int(input("Target percent vaccinated: "))
-
 
     # TODO 2: Call days_to_target and store result in a variable.
     a = days_to_target(population, doses, doses_per_day, target)
@@ -26,16 +24,22 @@ def main() -> None:
 
 # TODO 1: Define days_to_target function
 def days_to_target(population: int, doses: int, doses_per_day: int, target: int) -> int:
-    """A function to calculate the number of days to vaccination"""
+
+    
+    """A function to calculate the number of days to vaccination."""
     days = int(round((target / int(100) * population * int(2)) - doses) / doses_per_day)
     return(days)
+
 # TODO 3: Define future_date function
 def future_date(a: int) -> str:
-    """A function to calculate the date of target vaccination"""
+
+
+    """A function to calculate the date of target vaccination."""
     today: datetime = datetime.today()
     plus_days: timedelta = timedelta(a)
     date_raw: datetime = today + plus_days
     date = date_raw.strftime("%B %d, %Y")
     return(date)
+
 if __name__ == "__main__":
     main()
