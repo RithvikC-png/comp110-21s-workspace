35265bf59b5120f72939cbef7bf3850fdd264e32
RithvikC <rithvikc@ad.unc.edu>
2021-02-18 23:25:22 -0500

    complete p3

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -6,6 +6,7 @@ __author__ = "730398645"
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
+
     population: int = int(input("Population: "))
     doses: int = int(input("Doses administered: "))
     doses_per_day: int = int(input("Doses per day: "))
@@ -22,24 +23,25 @@ def main() -> None:
     z = " days which falls on " + str(b)
     print(x + y + z)
 
+
 # TODO 1: Define days_to_target function
 def days_to_target(population: int, doses: int, doses_per_day: int, target: int) -> int:
-
-    
     """A function to calculate the number of days to vaccination."""
+
     days = int(round((target / int(100) * population * int(2)) - doses) / doses_per_day)
     return(days)
 
+
 # TODO 3: Define future_date function
 def future_date(a: int) -> str:
-
-
     """A function to calculate the date of target vaccination."""
+
     today: datetime = datetime.today()
     plus_days: timedelta = timedelta(a)
     date_raw: datetime = today + plus_days
     date = date_raw.strftime("%B %d, %Y")
     return(date)
+
 
 if __name__ == "__main__":
     main()
