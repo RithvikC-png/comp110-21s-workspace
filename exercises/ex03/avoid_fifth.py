fab0608c5e1f00701369cc2f19633541a90e92eb
RithvikC <rithvikc@ad.unc.edu>
2021-03-08 09:44:05 -0500

    created input

--- exercises/ex03/avoid_fifth.py
+++ exercises/ex03/avoid_fifth.py
@@ -1,12 +1,17 @@
 """EX03 - avoid_fifth function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730398645"
 
 
 def main() -> None:
     """Entrypoint of the program."""
+    input_sentence: str = input("What would you like your sentence to be? ")
     # Put print statements here to test your function
+    avoid_fifth(input_sentence)
     # ex. print(avoid_fifth("hello there"))
+    print(f"Original Sentece: {input_sentence}")
+    
+    return(input_sentence)
 
 
 if __name__ == "__main__":
