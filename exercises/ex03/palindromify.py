ff81310e91aec4fe47ea20f7e3d28807a53f33a0
RithvikC <rithvikc@ad.unc.edu>
2021-03-08 18:00:54 -0500

    even works

--- exercises/ex03/palindromify.py
+++ exercises/ex03/palindromify.py
@@ -1,13 +1,36 @@
 """EX03 - palindromify function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730398645"
 
+original: str = input("What would you like to Palindromify? ")
+odd_even: bool = (len(original) % 2)
+
+if odd_even == 0:
+    x = True
+else:
+    x = False
 
 def main() -> None:
     """Entrypoint of the program."""
     # Put print statements here to test your function
     # ex. print(palindromify("race", false))
+    print(palindromify(original, x))
 
+
+def palindromify(original: str, x: bool) -> str:
+    """A way to palindrome inputs."""
+    
+    if x is True:
+        palindrome = original[::-1]
+        return (original + palindrome)
+
+    i = len(original)
+    if x is False:
+        while i > 0:
+            palindrome_odd = original[i]
+            i -= i
+    
+    return original + original[i]
 
 if __name__ == "__main__":
     main()
