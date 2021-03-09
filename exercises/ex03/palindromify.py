e56627fc9a5b4353f03f343930426ff639ad53f3
RithvikC <rithvikc@ad.unc.edu>
2021-03-08 23:36:53 -0500

    im trying

--- exercises/ex03/palindromify.py
+++ exercises/ex03/palindromify.py
@@ -6,9 +6,9 @@ original: str = input("What would you like to Palindromify? ")
 odd_even: bool = (len(original) % 2)
 
 if odd_even == 0:
-    x = True
-else:
     x = False
+else:
+    x = True
 
 def main() -> None:
     """Entrypoint of the program."""
@@ -19,13 +19,14 @@ def main() -> None:
 
 def palindromify(original: str, x: bool) -> str:
     """A way to palindrome inputs."""
+    if x is False:
+        new_statement = original[::-1]
+        return(original[0:len(original) -1 :1] + new_statement)
+        
     if x is True:
         palindrome = original[::-1]
         return (original + palindrome)
 
-    if x is False:
-        new_statement = original[::-1]
-    return(original[0:len(original) -1 :1] + new_statement)
 
 if __name__ == "__main__":
     main()
--- exercises/ex03/primes.py
+++ exercises/ex03/primes.py
@@ -1,13 +1,28 @@
 """EX03 - prime functions."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730398645"
 
 
 def main() -> None:
     """Entrypoint of the program."""
+    number = (int(input("What number would you like to test? ")))
     # Put print statements here to test your function
     # ex. print(is_prime(5)), print(list_primes(10, 20))
+    print(is_prime(number))
 
+def is_prime(number: int) -> bool:
+    numbers = range (2 , number)
+    if number > 1:
+        i: int = 0
+        while i < len(numbers):
+            if (number % numbers[i]) != 0:
+                return False
+                i += 1
+            if (number % numbers[i]) == 0:
+                return True
+    if number <= 1:
+        print("Composited")
+        return False
 
 if __name__ == "__main__":
     main()
