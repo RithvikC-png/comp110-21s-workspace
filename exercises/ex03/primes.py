b4ccb207ed8ab986a5c98971f9c74b03a02a3160
RithvikC <rithvikc@ad.unc.edu>
2021-03-09 02:03:07 -0500

    done

--- exercises/ex03/primes.py
+++ exercises/ex03/primes.py
@@ -15,15 +15,15 @@ def is_prime(x: int) -> bool:
     """Talk prime to me!"""
     numbers = range(2, x)
     if x > 1:
-            i: int = 0
-            while i < len(numbers):
-                check: int = numbers[i]
-                if (x % check) != 0:
-                    i += 1
-                if (x % check) == 0:
-                    return False
-            if i == len(numbers):
-                return True
+        i: int = 0
+        while i < len(numbers):
+            check: int = numbers[i]
+            if (x % check) != 0:
+                i += 1
+            if (x % check) == 0:
+                return False
+        if i == len(numbers):
+            return True
     if x <= 1:
         return False
     return True
