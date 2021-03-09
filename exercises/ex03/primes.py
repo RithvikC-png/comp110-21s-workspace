c2d4c39a17aca353a3141082e3b83973e85978ed
RithvikC <rithvikc@ad.unc.edu>
2021-03-09 00:34:53 -0500

    im still trying

--- exercises/ex03/primes.py
+++ exercises/ex03/primes.py
@@ -6,15 +6,20 @@ __author__: str = "730398645"
 def main() -> None:
     """Entrypoint of the program."""
     number = (int(input("What number would you like to test? ")))
+    lower_number = (int(input("What is your lower number? ")))
+    upper_number = (int(input("What is your upper number? ")))
     # Put print statements here to test your function
     # ex. print(is_prime(5)), print(list_primes(10, 20))
     print(is_prime(number))
+    print(list_prime(lower_number, upper_number))
 
 
 def is_prime (number: int) -> bool:
     """Talk prime to me!"""
-    numbers = range (1, number)
+    numbers = range (2, number)
     if number > 1:
+        if number == 2:
+            return False
         i: int = 0
         while i < len(numbers):
             check: int = numbers[i]
@@ -25,6 +30,15 @@ def is_prime (number: int) -> bool:
                 return False
     if number <= 1:
         return False
+
+def list_prime(lower_number: int, upper_number: int) -> list[int]:
+    """What are the prime numbers in this range?"""
+    prime_numbers = []
+    while(lower_number < upper_number):
+        if is_prime(lower_number):
+            prime_numbers.append(lower_number)
+        lower_number += 1
+    return prime_numbers
 
 
 if __name__ == "__main__":
