86a485e9a67ec83689940e7177e5861d87533499
RithvikC <rithvikc@ad.unc.edu>
2021-03-24 01:33:55 -0400

    3

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -4,22 +4,24 @@ __author__ = "730398645"
 
 
 from csv import DictReader
-from data import nc_durham_2015_march_21_to_27 as csv_file
+DATA_DIRECTORY="../../data"
+DATA_FILE_PATH=f"{DATA_DIRECTORY}/nc_durham_2015_march_21_to_27.csv"
 
 file_handle = open("data/nc_durham_2015_march_21_to_27.csv", "r", encoding="utf8")
-csv_reader = DictReader(file_handle)
+csv_file = DictReader(file_handle)
 
 def read_csv_rows(csv_file: str) -> list[dict[str, str]]:
     """Read a CSV file's contents into a list of rows."""
     rows: list[dict[str, str]] = []
 
-    for row in csv_reader:
-        print(row)
+    for row in csv_file:
+        rows.append(row)
 
+    return(rows)
     file_handle.close()
     
     # TODO 0.1) Complete the implementation of this function here.
 
-read_csv_rows(csv_file)
+print(read_csv_rows(csv_file))
 
 # TODO: Define the other functions here.
