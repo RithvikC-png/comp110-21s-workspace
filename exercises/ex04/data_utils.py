9922e22429cf12debee4166a7cc4a06d4a0468e5
RithvikC <rithvikc@ad.unc.edu>
2021-03-25 03:09:27 -0400

    19

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -29,7 +29,6 @@ def column_values(rows: list[dict[str, str]], column: str) -> list[str]:
 
 def columnar(rows: list[dict[str, str]]) -> dict[str, list[str]]:
     """Convert rows into columns."""
-    print(column_values(rows, column))
     headers: dict[str, list[str]] = {}
     for row in rows[0]:
         columns = {"subject_age": [charts]}
