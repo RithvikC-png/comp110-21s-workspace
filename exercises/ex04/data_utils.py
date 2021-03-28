1aa947eddaba4f715020b37e41117cc600042f85
RithvikC <rithvikc@ad.unc.edu>
2021-03-28 00:31:08 -0400

    1.0.25

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -42,7 +42,7 @@ def head(columns: dict[str, list[str]], rows: int) -> dict[str, list[str]]:
     Dictionary_A: dict[str, list[str]] = {}
     N: list[str] = []
     for column in columns:
-        for rows in column:
+        for row in column:
             N.append(row)
         Dictionary_A[column] = N
     return Dictionary_A
