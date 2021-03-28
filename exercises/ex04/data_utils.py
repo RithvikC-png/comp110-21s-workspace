2b00ac3341779fa8e0cd155eb408bf38baa86d1f
RithvikC <rithvikc@ad.unc.edu>
2021-03-28 00:27:32 -0400

    1.0.21

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -40,7 +40,7 @@ def columnar(rows: list[dict[str, str]]) -> dict[str, list[str]]:
 
 def head(columns: dict[str, list[str]], rows: int) -> dict[str, list[str]]:
     Dictionary_A: dict[str, list[str]] = {}
-    for column in rows:
+    for rows in columns:
         N: list[str] = []
         for value in column:
             N.append(value)
